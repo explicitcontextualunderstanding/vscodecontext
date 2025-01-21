a4aff33c3b6b7f11befebf60daa1e3886f7cfe7a
Explicit Contextual Understanding <explicitcontextualunderstanding@gmail.com>
2025-01-21 14:22:10 -0800

    refactor: use import type for type-only imports in IExtensionContext

---
+++ src/interfaces/IExtensionContext.ts
@@ -0,0 +1,9 @@
+import type { Disposable } from 'vscode';
+
+export interface IExtensionContext {
+  /**
+   * An array to which disposables can be added. When this extension is
+   * deactivated the disposables will be disposed.
+   */
+  readonly subscriptions: Disposable[];
+}
